4261460493405b221bbb5848c4d2b1b1a6120df4
MXNXV <angbymtg@gmail.com>
2022-06-15 21:12:43 +0530

    .

--- kruskal.cpp
+++ kruskal.cpp
@@ -1,26 +1,28 @@
 #include<stdio.h>//Standard input output
 int main()//Main Function
 {
-	int i,j,n,ne,min_cost=0,min,source;
-	printf("Enter the number of nodes");
+	int i,j,n,ne,a,b,min_cost=0,min;
+	printf("Enter the number of nodes\n");
 	scanf("%d",&n);//Input number of nodes
 	printf("Enter cost matrix\n");
-	int cost[n][n];
-	for(i=1;i<=n;i++)
+	int cost[n][n],parent[n];
+	for(i=1;i<=n;parent[i++]=0)
 		for(j=1;j<=n;j++)
 			scanf("%d",&cost[i][j]);//Input cost matrix
 	printf("Minimum cost spanning tree is\n");
  	for(ne=1;ne<n;min=999)
-		for(i=1;i<=n;i++)
+	{	for(i=1;i<=n;i++)
 			for(j=1;j<=n;j++)
 				if(cost[i][j]<min)//Checking if it is the minimum edge
-					{	min=cost[i][j];//Finding the minimum edge
-						if(i!=j)//Checking for cycles
-						{	printf("\nEdge%d\t(%d->%d)=%d",ne++,i,j,min);
-							min_cost+=min;//Adding minimum edge cost to sum
-						}
-						cost[i][j]=cost[j][i]=999;//Disconnecting the edge
-												//to avoid re-calculation
-					}
+					min=cost[a=i][b=j];//Finding the minimum edge		
+		while(parent[a])a=parent[a];
+		while(parent[b])b=parent[b];
+		if(a!=b)//Checking for cycles
+		{	printf("\nEdge%d\t(%d->%d)=%d",ne++,a,b,min);
+			min_cost+=min;//Adding minimum edge cost to sum
+			parent[b]=a;
+		}
+		cost[a][b]=cost[b][a]=999;//Disconnecting the edge
+	}											//to avoid re-calculation
 	printf("\nMinimum cost %d \n",min_cost);//Printing the minimum cost
 }
--- prims.c
+++ prims.c
@@ -1,7 +1,7 @@
 #include<stdio.h>//Standard input output
 int main()//Main Function
 {
-	int i,j,n,ne,min_cost=0,min,source;
+	int i,j,n,ne,min_cost=0,min,source,a,b;
 	printf("Enter the number of nodes");
 	scanf("%d",&n);//Input number of nodes
 	printf("Enter cost matrix\n");
@@ -14,18 +14,18 @@ int main()//Main Function
 	visited[source]=1;
 	printf("Minimum cost spanning tree is\n");
  	for(ne=1;ne<n;min=999)
-		for(i=1;i<=n;i++)
+	{	for(i=1;i<=n;i++)
 			for(j=1;j<=n;j++)
 				if(cost[i][j]<min)//Checking if it is the minimum edge
 					if(visited[i]==1)//Checking if node already visited
-					{	min=cost[i][j];//Finding the minimum edge
-						if(visited[i]==0||visited[j]==0)//Checking for cycles
-						{	printf("\nEdge%d\t(%d->%d)=%d",ne++,i,j,min);
-							min_cost+=min;//Adding minimum edge cost to sum
-							visited[j]=1;//Make the vertex as visited
-						}
-						cost[i][j]=cost[j][i]=999;//Disconnecting the edge
-												//to avoid re-calculation
-					}
+						min=cost[a=i][b=j];//Finding the minimum edge
+											//to avoid re-calculation
+		if(visited[a]==0||visited[b]==0)//Checking for cycles
+		{	printf("\nEdge%d\t(%d->%d)=%d",ne++,a,b,min);
+			min_cost+=min;//Adding minimum edge cost to sum
+			visited[b]=1;//Make the vertex as visited
+		}
+		cost[a][b]=cost[b][a]=999;//Disconnecting the edge
+	}
 	printf("\nMinimum cost %d \n",min_cost);//Printing the minimum cost
 }
