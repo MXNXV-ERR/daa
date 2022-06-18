9451080062d9c1872689c7694c151f04dbcb272e
MXNXV <angbymtg@gmail.com>
2022-06-18 11:52:47 +0530

    .

--- kruskal.c
+++ kruskal.c
@@ -1,7 +1,7 @@
 #include<stdio.h>//Standard input output
 int main()//Main Function
 {
-	int i,j,n,ne,a,b,min_cost=0,min;
+	int i,j,n,ne,a,b,u,v,min_cost=0,min=999;
 	printf("Enter the number of nodes\n");
 	scanf("%d",&n);//Input number of nodes
 	printf("Enter cost matrix\n");
@@ -14,15 +14,15 @@ int main()//Main Function
 	{	for(i=1;i<=n;i++)
 			for(j=1;j<=n;j++)
 				if(cost[i][j]<min)//Checking if it is the minimum edge
-					min=cost[a=i][b=j];//Finding the minimum edge		
+					min=cost[u=a=i][v=b=j];//Finding the minimum edge		
 		while(parent[a])a=parent[a];
 		while(parent[b])b=parent[b];
 		if(a!=b)//Checking for cycles
-		{	printf("\nEdge%d\t(%d->%d)=%d",ne++,a,b,min);
+		{	printf("\nEdge%d\t(%d->%d)=%d",ne++,u,v,min);
 			min_cost+=min;//Adding minimum edge cost to sum
 			parent[b]=a;
 		}
-		cost[a][b]=cost[b][a]=999;//Disconnecting the edge
+		cost[u][v]=cost[v][u]=999;//Disconnecting the edge
 	}								//to avoid re-calculation
 	printf("\nMinimum cost %d \n",min_cost);//Printing the minimum cost
 }
